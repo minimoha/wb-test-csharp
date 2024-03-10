43a27c2616c89b00cd07e4ed789e974bbeb4f843
muhammed <muhammed>
2024-03-10 08:42:00 -0700

    updates

--- TestApi/Data/AppDbContext.cs
+++ TestApi/Data/AppDbContext.cs
@@ -1,13 +1,79 @@
 ﻿
+using System.Threading;
+
 namespace TestApi.Data;
 
-    public class AppDbContext : DbContext
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<OtpHistory> OtpHistory { get; set; }
 
     }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<OtpHistory> OtpHistory { get; set; }
+    public DbSet<Audit> AuditLogs { get; set; }
+
+
+    public override int SaveChanges()
+    {
+        BeforeSaveChanges();
+        return base.SaveChanges();
+    }
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        BeforeSaveChanges();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void BeforeSaveChanges()
+    {
+        ChangeTracker.DetectChanges();
+        var auditEntries = new List<AuditEntry>();
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged)
+                continue;
+            var auditEntry = new AuditEntry(entry);
+            auditEntry.TableName = entry.Entity.GetType().Name;
+            auditEntries.Add(auditEntry);
+            foreach (var property in entry.Properties)
+            {
+                string propertyName = property.Metadata.Name;
+                if (property.Metadata.IsPrimaryKey())
+                {
+                    auditEntry.KeyValues[propertyName] = property.CurrentValue;
+                    continue;
+                }
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        auditEntry.AuditType = AuditType.Create;
+                        auditEntry.NewValues[propertyName] = property.CurrentValue;
+                        auditEntry.User = "SYSTEM";
+                        break;
+                    case EntityState.Deleted:
+                        auditEntry.AuditType = AuditType.Delete;
+                        auditEntry.OldValues[propertyName] = property.OriginalValue;
+                        auditEntry.User = "SYSTEM";
+                        break;
+                    case EntityState.Modified:
+                        if (property.IsModified)
+                        {
+                            auditEntry.ChangedColumns.Add(propertyName);
+                            auditEntry.AuditType = AuditType.Update;
+                            auditEntry.OldValues[propertyName] = property.OriginalValue;
+                            auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            auditEntry.User = "SYSTEM";
+                        }
+                        break;
+                }
+            }
+        }
+        foreach (var auditEntry in auditEntries)
+        {
+            AuditLogs.Add(auditEntry.ToAudit());
+        }
+    }
+
+
+}
---
+++ TestApi/Models/AuditLog.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApi.Models
+{
+    public enum AuditType
+    {
+        Create = 1,
+        Update = 2,
+        Delete = 3
+    }
+
+    public class Audit
+    {
+        public long Id { get; set; }
+        public string User { get; set; }
+        public string Type { get; set; }
+        public string TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string OldValues { get; set; }
+        public string NewValues { get; set; }
+        public string AffectedColumns { get; set; }
+        public string PrimaryKey { get; set; }
+    }
+}
